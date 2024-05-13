4e3f2c23f88042cc7024d37096c52de4dfd90252
G N <gracecenat2@gmail.com>
2024-05-13 16:53:22 +0700

    lanjutan GA

--- main.m
+++ main.m
@@ -3,9 +3,21 @@ target = 'grace'
 panjang = length(target);
 genes = create_genes(panjang)
 
-% random_number = randi([32,126],1,panjang);
-% genes = char(random_number);
-% genes
 
 % fitnes
 fitnes = calculate_fitnes(genes,target)
+
+% Populasi
+population_size = 10;
+population = create_population(target,population_size)
+
+% Selection
+[parent1, parent2] = selection(population)
+
+% crossover
+[child1,child2] = crossover(parent1,parent2)
+
+%  Mutation
+mutation_rate = 0.5;
+mutant1 = mutation(child1,mutation_rate)
+mutant2 = mutation(child2,mutation_rate)
